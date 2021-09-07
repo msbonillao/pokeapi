904be53694ee6d9a29f5e4bf7ca0dbc97c5b836a
Michael Bonilla <michael.bonilla@globant.com>
2021-09-06 23:00:07 -0500

    ,

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 import os
 from multiprocessing import cpu_count
 
-bind = "localhost:{}".format(os.environ.get("SERVER_PORT", "5124"))
+bind = "0.0.0.0:{}".format(os.environ.get("SERVER_PORT", "5124"))
 
