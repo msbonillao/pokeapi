6670e50b5f4f579a347d6f24584d44ddd9b754ec
Michael Bonilla <michael.bonilla@globant.com>
2021-09-06 23:27:07 -0500

    Redis

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
